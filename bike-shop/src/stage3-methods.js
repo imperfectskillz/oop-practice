60ad923ea1cc35116f74cefe88cae332bb0968e6
James Choi <jameschoi@Jamess-MacBook-Pro.local>
2017-05-11 10:06:34 -0700

    stage3 complete

--- bike-shop/src/stage3-methods.js
+++ bike-shop/src/stage3-methods.js
@@ -1,13 +1,46 @@
 function Bike() {
-  // your code here
+  this._isMoving = false;
+  this.rings = [3,7];
+  this.tires = [new Tire(), new Tire()];
 }
+
+Bike.prototype.isMoving = function () {
+  return this._isMoving;
+}
+Bike.prototype.pedal = function() {
+  this._isMoving = true;
+  if (this.tires[0]._isFlat || this.tires[1]._isFlat) {
+    throw("Can't pedal with a flat tire!");
+  }
+
+Bike.prototype.brake = function() {
+  this._isMoving = false;
+}
+
+Bike.prototype.gearSpeeds = function() {
+  return parseInt(this.rings[0]) * parseInt(this.rings[1]);
+}
+
+};
 
 function Frame() {
   // your code here
 }
 
 function Tire() {
-  // your code here
+  this._isFlat = false;
+}
+
+Tire.prototype.isFlat = function() {
+  return this._isFlat;
+}
+
+Tire.prototype.puncture = function() {
+  this._isFlat = true;
+}
+
+Tire.prototype.repair = function() {
+  this._isFlat = false;
 }
 
 module.exports = {
